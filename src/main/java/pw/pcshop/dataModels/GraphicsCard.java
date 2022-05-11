c66bbaeb5494037b02c26d784f895c26d42a4e8d
Dominik Wawrzyniuk <dominikwawrzyniuk@gmail.com>
2022-05-11 20:55:27 +0200

    Added graphics cards

---
+++ src/main/java/pw/pcshop/dataModels/GraphicsCard.java
@@ -0,0 +1,40 @@
+package pw.pcshop.dataModels;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "graphicsCards")
+public class GraphicsCard {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String manufacturer;
+
+    @Column
+    private String series;
+
+    @Column
+    private int vRAM;
+
+    @Column
+    private String memoryType;
+
+    @Column
+    private int length;
+
+    @Column
+    private int width;
+
+    @Column
+    private int height;
+}
