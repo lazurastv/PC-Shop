8c21c0b1e48204fc690ebeb41bde98445fc579c7
Dominik Wawrzyniuk <dominikwawrzyniuk@gmail.com>
2022-05-11 20:51:09 +0200

    Added motherboard

---
+++ src/main/java/pw/pcshop/dataModels/Motherboard.java
@@ -0,0 +1,34 @@
+package pw.pcshop.dataModels;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "motherboards")
+public class Motherboard {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String manufacturer;
+
+    @Column
+    private String series;
+
+    @Column
+    private String size;
+
+    @Column
+    private String socket;
+
+    @Column
+    private String maxRAM;
+}
