a282d8e5b132164b16e7343e39a54bbe1c5a9752
Dominik Wawrzyniuk <dominikwawrzyniuk@gmail.com>
2022-05-11 21:17:51 +0200

    Added relationship to user

--- src/main/java/pw/pcshop/dataModels/Computer.java
+++ src/main/java/pw/pcshop/dataModels/Computer.java
@@ -6,6 +6,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -18,6 +19,10 @@ public class Computer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 
     @OneToOne
     @JoinColumn(name = "processor_id")
--- src/main/java/pw/pcshop/dataModels/User.java
+++ src/main/java/pw/pcshop/dataModels/User.java
@@ -1,12 +1,14 @@
 package pw.pcshop.dataModels;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.Data;
@@ -18,6 +20,9 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @OneToMany(mappedBy = "user")
+    private List<Computer> computers;
 
     @Column
     private String name;
