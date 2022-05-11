8044fa32ee89cb43ef7f3b891a10b16ba0939d6b
Dominik Wawrzyniuk <dominikwawrzyniuk@gmail.com>
2022-05-11 19:23:08 +0200

    Added processor entity

---
+++ src/main/java/pw/pcshop/dataModels/Processor.java
@@ -0,0 +1,31 @@
+package pw.pcshop.dataModels;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "processors")
+public class Processor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String company;
+
+    @Column
+    private String series;
+
+    @Column
+    private int threadCount;
+
+    @Column
+    private double frequency;
+}
