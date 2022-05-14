0dcf80f13237230573a07427a6eec129510b0dc5
Dominik Wawrzyniuk <dominikwawrzyniuk@gmail.com>
2022-05-14 17:55:24 +0200

    Added request body annotations

--- src/main/java/pw/pcshop/controllers/GraphicsCardController.java
+++ src/main/java/pw/pcshop/controllers/GraphicsCardController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,7 +27,7 @@ public class GraphicsCardController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void add(GraphicsCardVM graphicsCardVM) {
+    public void add(@RequestBody GraphicsCardVM graphicsCardVM) {
         graphicsCardService.add(graphicsCardVM);
     }
 }
--- src/main/java/pw/pcshop/controllers/UserController.java
+++ src/main/java/pw/pcshop/controllers/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,7 +27,7 @@ public class UserController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void add(UserVM userVM) {
+    public void add(@RequestBody UserVM userVM) {
         userService.add(userVM);
     }
 }
