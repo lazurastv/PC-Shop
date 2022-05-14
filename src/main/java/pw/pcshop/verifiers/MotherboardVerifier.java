9a67cfbc0e4120f8147a07d39e47dda0c1c63bfd
Dominik Wawrzyniuk <dominikwawrzyniuk@gmail.com>
2022-05-14 14:52:11 +0200

    Hooked up motherboard verifier

--- src/main/java/pw/pcshop/verifiers/MotherboardVerifier.java
+++ src/main/java/pw/pcshop/verifiers/MotherboardVerifier.java
@@ -1,6 +1,19 @@
 package pw.pcshop.verifiers;
 
+import pw.pcshop.addModels.AddMotherboard;
+
 public class MotherboardVerifier {
+    public VerificationResult verifyMotherboard(AddMotherboard motherboard) {
+        try {
+            VerifierUtils.throwIfNotCorrect(verifyManufacturer(motherboard.manufacturer));
+            VerifierUtils.throwIfNotCorrect(verifySize(motherboard.size));
+            VerifierUtils.throwIfNotCorrect(verifyMaxRAM(motherboard.maxRAM));
+        } catch (VerificationException v) {
+            return new VerificationResult(v.getMessage());
+        }
+        return new VerificationResult();
+    }
+
     private VerificationResult verifyManufacturer(String value) {
         return VerifierUtils.verifyInList("Manufacturer", new String[] { "Asus", "MSI", "Gigabyte", "ASRock", "Acer" },
                 value);
